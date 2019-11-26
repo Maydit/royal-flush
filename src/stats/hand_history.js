e6fc09d09dbd2f229e3883b329a4f069000a976e
Justin <justinchenchen673@gmail.com>
2019-11-25 20:45:45 -0500

    refactored stats folder

--- src/stats/hand_history.js
+++ src/stats/hand_history.js
@@ -2,21 +2,11 @@ const App = new Vue({
     el: '#app',
     beforeMount() {
         this.$http.get('http://' + window.location.host + '/getHandHistory').then(response => {
-            console.log(response);
             for (var i = 0; i < response.body.length; i++) {
+                // To create the unicode card
                 var card1 = convertToUni(response.body[i].substring(0, 2));
                 var card2 = convertToUni(response.body[i].substring(2, 4));
-
-
-
-
-
-
-
-
-
-
-
+                // Create new HTML row
                 document.getElementById("table").innerHTML += "<tr><td>" + card1 + "</td><td>" + card2 + "</td><td>" + response.body[i].substring(5) +"</td></tr>";
             }
         });
