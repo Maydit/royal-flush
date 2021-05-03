ddebd7b38c0d09e5e3021dd887b7a2e2b2a86620
andyster17 <andyster178@gmail.com>
2021-05-03 00:47:12 -0400

    Add files via upload
    working version of testing, it no longer makes toxic accounts

--- testing.py
+++ testing.py
@@ -5,31 +5,44 @@ from selenium.webdriver.support.wait import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import requests
+from bs4 import BeautifulSoup
+import os 
 import time
 from pokereval.card import Card
 from pokereval.hand_evaluator import HandEvaluator
 
 globalRankTranslation = {'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':11,'Q':12,'K':13,'A':14}
 globalSuitTranslation = {'S':1,'H':2,'D':3,'C':4}
-
+def interceptor(request):
+    del request.headers['Referer']  # Delete the header first
+    request.headers['Referer'] = 'some_referer'
 class User: 
     def __init__(self,firstName, lastName, email, password):
         self.firstName = firstName
         self.lastName = lastName
         self.email = email
         self.password = password
-        self.driver =webdriver.Chrome(ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--incognito')
+        options.add_argument('user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"')
+        self.driver =webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        self.driver.request_interceptor = interceptor
+        curURL=self.driver.current_url
         self.driver.get("http://localhost:3000")
-        time.sleep(2)
+        wait(self.driver,15).until(EC.url_changes(curURL))
+
         self.driver.maximize_window()
         try:
-            #self.register()
+            self.register()
             self.login()
         except:
-            curURL=self.driver.current_url
-            self.driver.back()
-            wait(self.driver,15).until(EC.url_changes(curURL))
-            self.login()
+            #curURL=self.driver.current_url
+            #self.driver.back()
+            #wait(self.driver,15).until(EC.url_changes(curURL))
+            #self.login()
+            pass
     
     def login(self):
         curURL=self.driver.current_url
@@ -39,6 +52,14 @@ class User:
         self.driver.find_element_by_id("pass1").send_keys(self.password)
         assert(self.driver.find_element_by_id("submit_reg").is_enabled())
         self.driver.find_element_by_id("submit_reg").click()
+        for request in self.driver.requests:
+            if request.response:
+                print(
+                    request.url,
+                    request.response.status_code,
+                    request.response.headers['Content-Type']
+                )
+        self.driver.wait_for_request(timeout=10)
         time.sleep(10)
 
 
@@ -49,11 +70,7 @@ class User:
         self.driver.find_element_by_id("pass1").send_keys(self.password)
         self.driver.find_element_by_id("pass2").send_keys(self.password)
         assert(self.driver.find_element_by_id("submit_reg").is_enabled())
-        curURL=self.driver.current_url
         self.driver.find_element_by_id("submit_reg").click()
-        wait(self.driver,15).until(EC.url_changes(curURL))
-        time.sleep(2)
-
     def logout(self):
         pass
     def createGame(self):
@@ -115,7 +132,10 @@ class Poker:
 
 
 if __name__ == "__main__":
-    andy= User("Andy","C","andy@gmail.com","1234")
-    fred=User("Fred","L","fred@gmail.com","1234")
-    jeff = User("Jeff","M","jeff@gmail.com","1234")
-    jinnthon = User("Jinny","H","Jin@f.com","1234")
+    andy= User("Andy","C","andy154544@gmail.com","1234")
+    fred=User("Fred","L","fred124124@gmail.com","1234")
+    jeff = User("Jeff","M","jeff1241224@gmail.com","1234")
+    jinnthon = User("Jinny","H","Jin124124@f.com","1234")
+
+
+    
